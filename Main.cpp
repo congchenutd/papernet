a2f93154e007ca3004fa074598b87f35b9e3e694
Cong Chen <Cong@Congs-MBP15.local>
2016-01-25 14:27:21 -0800

    + Hi-DPI icons

--- Main.cpp
+++ Main.cpp
@@ -10,9 +10,27 @@ extern QString attachmentDir;
 extern QString emptyDir;
 extern QString backupDir;
 
+// workaround for a bug on Mavericks
+// Finder returns / as the working path of an app bundle
+// but if the app is run from terminal, the path is correct
+// This method calcluates the path of the bundle from the application's path
+QString getCurrentPath()
+{
+    QDir dir(QApplication::applicationDirPath());
+    dir.cdUp();
+    dir.cdUp();
+    dir.cdUp();
+    return dir.absolutePath();
+}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+#ifdef Q_OS_OSX
+    QDir::setCurrent(getCurrentPath());
+#endif
 
     // the database cannot be accessed by multiple instances
     SingleInstance singleInstance("PaperNet");
